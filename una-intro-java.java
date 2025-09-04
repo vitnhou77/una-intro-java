905aca20c73d7af6752a428c95d4b9c1b075eeef
Vitor Hugo Pereira <vitorhugopereirasilva12@gmail.com>
2025-09-04 09:41:34 -0300

    una-intro-java.java

--- una-intro-java.java
+++ una-intro-java.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
-public class Main
+public class Main 
 {
-	public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
-	    
-		System.out.println("Digite o número 1");
-		int numero1 = scanner.nextInt();
-		
-		System.out.print("Digite o número 2");
-        int numero2 = scanner.nextInt();
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        int soma = numero1 + numero2;
+        System.out.println("Digite um número inteiro: ");
+        int num1 = scanner.nextInt();
         
-        System.out.println("A soma dos dois números é:" + soma);
+        System.out.println("Digite o segundo número inteiro: ");
+        int num2 = scanner.nextInt();
+        
+        int soma = num1 + num2;
+        
+        System.out.println("A soma dos números é: " + soma);
         
         scanner.close();
-
-	}
+    }
 }
